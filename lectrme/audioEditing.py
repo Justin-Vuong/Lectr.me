3d130955430093964c534c623b476d6aa8a7f953
Justin Vuong <vuong.justin01@gmail.com>
2020-01-26 05:12:01 -0500

    added playback speed variation

--- lectrme/audioEditing.py
+++ lectrme/audioEditing.py
@@ -3,18 +3,28 @@ import argparse
 from pydub import AudioSegment
 from pydub.playback import play
 from pydub.silence import split_on_silence
-import pydub.audio_segment as Audio
+import pydub.audio_segment
 import pydub.pyaudioop
+from pydub.effects import speedup
 
 parser = argparse.ArgumentParser()
-parser.add_argument("filename")
-parser.add_argument("environment_sound", type = str, nargs='?', default="moderate")
+parser.add_argument("filename", help = "What is the name of the file with the .mp3 extension")
+parser.add_argument("environment_sound", type = str, nargs='?', default="moderate", help = "How much background noise is there? [low,moderate,high]")
+parser.add_argument("playback_speed", type = float, nargs='?', default=1.0, help = "How fast do you want to play the track? 0.5 is 50% speed")
 args = parser.parse_args()
 
 
 def audioInput(inFileName):
     sound = AudioSegment.from_file(inFileName, format="mp3")
     return sound
+
+def playback_rate(track, speed=1.0):
+
+    alteredFile = track._spawn(track.raw_data, overrides={
+         "frame_rate": int(track.frame_rate * speed)
+      })
+    return alteredFile.set_frame_rate(track.frame_rate)
+
 
 editFile = audioInput(args.filename)
 minDB_silence = -20
@@ -34,11 +44,13 @@ except:
 
 compression = int(100.0* round(1.0 - (float(len(outputFile))/float(len(editFile))),4))
 print("Trying with silence at " + str(minDB_silence) + "dB and the compression is " + str(compression))
-maxCompression = compression
 exportFile = outputFile
 
+if(args.playback_speed != 1):
+    newExport = speedup(exportFile, args.playback_speed, 150, 25)
+    newExport.export("Cut_"+ args.filename, format='mp3')
+else:
+    exportFile.export("Cut_"+ args.filename, format='mp3')
 
-
-print("Exporting...")
-exportFile.export("Cut_"+"_"+ args.filename, format='mp3')
-print("Done. Cut_" + args.filename + " was cut down by " + str(maxCompression) + "%")
+print("Done. Cut_" + args.filename + " was cut down by " + str(compression) + "%")
+print("Cut_"+ args.filename)
