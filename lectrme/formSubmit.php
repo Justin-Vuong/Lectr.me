d004c17196a0d93681de1d04c4fa905b29aca837
Keith <keithchoa@gmail.com>
2020-01-26 03:59:44 -0500

    debugging

--- lectrme/formSubmit.php
+++ lectrme/formSubmit.php
@@ -10,10 +10,10 @@ if ($file_type != 'mp3'){
     $upload_status = 0;
 }
 if ($upload_status === 0){
-    echo '<div class = "errorMessage">An error has occurred. </div>';
+    echo '<div class = "errorMessage">An error has occurred. Upload Status is 0.</div>';
 }
 else {
-    if(move_uploaded_file($_FILES["uploadedmp3"]["tmp_name"], $uploaded_name)) {
+    if(move_uploaded_file($_FILES["uploadedmp3"]["name"], $uploaded_name)) {
         echo basename($_FILES["uploadedmp3"]["name"]);
         echo '<div class = "uploadedMessage"> The file '. basename($_FILES["uploadedmp3"]["name"]). ' has been uploaded.</div>';
     }
