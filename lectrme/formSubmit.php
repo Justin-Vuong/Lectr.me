993f5f01b71c8f6f559edc509aa724a7474101a7
Keith <keithchoa@gmail.com>
2020-01-26 06:44:01 -0500

    ?

--- lectrme/formSubmit.php
+++ lectrme/formSubmit.php
@@ -10,10 +10,10 @@ $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
  if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
         shell_exec("cd uploads");
         $command = "python ../audioEditing.py ". $_FILES["file"]["name"];
-        //$_SESSION["debugvar"] = $command;
+        $_SESSION["debugvar"] = $command;
         $output = shell_exec($command);
         shell_exec("cd ..");
-        $_SESSION["debugvar"] = $output;
+        //$_SESSION["debugvar"] = $output;
         $_SESSION["cut_file"] = "uploads/Cut_". $output;
     } else {
         echo "Sorry, there was an error uploading your file.";
