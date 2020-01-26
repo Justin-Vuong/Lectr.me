4cb3f6c24581a9a26c501ee3aa9c16e79005b8b1
Benjamin <benjaminlu34@gmail.com>
2020-01-25 22:19:41 -0500

    fdfdfdfdf

--- lectrme/test.php
+++ lectrme/test.php
@@ -3,10 +3,13 @@ $target_directory = "uploads/";
 $uploaded_name = $target_directory.basename($_FILES["fileTest"]["name"]);
 $upload_status = 1;
 $file_type = strtolower(pathinfo($uploaded_name, PATHINFO_EXTENSION));
+$file_size = $_FILES["fileTest"]["size"];
 
-if (isset($_POST["submittest"])){
-
+if ($file_size > 400000000){
+    echo "File size can't exceed 400 MB.";
+    $upload_status = 0;
 }
+
 if ($file_type != "mp3"){
     echo "Only audio files are allowed to be uploaded. ";
     $upload_status = 0;
@@ -18,6 +21,6 @@ if ($upload_status === 0){
         echo basename($_FILES["fileTest"]["name"]);
         echo "The file ". basename($_FILES["fileTest"]["name"]). " has been uploaded.";
     } else {
-        echo "test test :(";
+        echo "An error has occurred.";
     }
 }
